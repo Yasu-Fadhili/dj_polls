f7936852129838ddb8fae7cee35f20fbfda597f8
Yasu Fadhili <yasufadhili@gmail.com>
2023-10-31 10:04:24 +0300

    Created polls app

--- dj_polls/settings.py
+++ dj_polls/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -92,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
