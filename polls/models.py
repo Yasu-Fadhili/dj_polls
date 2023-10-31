8669cb1af7b87f29afbec8d18a304f4bee0b69a5
Yasu Fadhili <yasufadhili@gmail.com>
2023-10-31 10:28:58 +0300

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -64,7 +64,7 @@ class Comment(models.Model):
     poll = models.ForeignKey("polls.Poll", verbose_name=_("Poll"), on_delete=models.CASCADE)
     author = models.ForeignKey(User, verbose_name=_("Poll Author"), on_delete=models.CASCADE)
     content = models.TextField(_("Poll Comment"), max_length=1000)
-    created_at =- models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
