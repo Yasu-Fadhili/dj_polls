5d8b6b13108e2ca9ea7411c66d6ba9d331ea0de6
Yasu Fadhili <yasufadhili@gmail.com>
2023-10-31 11:45:31 +0300

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import (
+    viewsets,
+    views,
+    permissions,
+    exceptions
+)
+
+from polls.serialisers import (
+    PollSerializer, OptionSerializer, VoteSerializer
+)
+
+from polls.models import (
+    Poll, Option, Vote
+)
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    serializer_class = PollSerializer
+    queryset = Poll.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def destroy(self, request, *args, **kwargs):
+        poll = Poll.objects.get(id=self.kwargs["id"])
+        if not request.user == poll.author:
+            raise exceptions.PermissionDenied("You can not delete this poll")
+        return super().destroy(request, *args, **kwargs)
+
