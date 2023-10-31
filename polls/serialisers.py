e215de0cae720e15742bddcdd66d70ebaad58f7d
Yasu Fadhili <yasufadhili@gmail.com>
2023-10-31 12:33:06 +0300

    Update serialisers.py

--- polls/serialisers.py
+++ polls/serialisers.py
@@ -8,30 +8,59 @@ from polls.models import (
     Poll,
     Option,
     Vote,
+    Comment
 )
 
 class VoteSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Vote
-        fields = "__all__"
+        fields = (
+            "id",
+            "author",
+            "poll",
+            "option"
+        )
 
 class OptionSerializer(serializers.ModelSerializer):
     votes = VoteSerializer(many=True, required=False)
 
     class Meta:
         model = Option
-        fields = "__all__"
+        fields = (
+            "id",
+            "option"
+        )
 
 class PollSerializer(serializers.ModelSerializer):
-    was_published_recently = serializers.BooleanField(read_only=True)
-    options = OptionSerializer(many=True, read_only=True, required=False)
+    #was_published_recently = serializers.BooleanField(read_only=True)
+    choices = OptionSerializer(many=True, read_only=True, required=False)
+    #choices = OptionSerializer()
 
     class Meta:
         model = Poll
-        fields = "__all__"
+        fields = (
+            "id",
+            "question",
+            "choices",
+            "expiry_date",
+            "visibility",
+            "created_at",
+            "updated_at"
+        )
     
     def create(self, validated_data):
         return Poll.objects.create(**validated_data)
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            "id",
+            "poll",
+            "author",
+            "content",
+            "created_at"
+        )
 
